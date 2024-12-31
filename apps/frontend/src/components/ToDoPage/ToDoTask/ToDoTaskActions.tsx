bb674796d19f19c2bd1f3cbe30ebc7cf67bef8fb
R3ygoski <hckranonymous2@hotmail.com>
2024-12-30 21:37:33 -0300

    feat(frontend): integration of Modal and To-Do Task Action button

--- apps/frontend/src/components/ToDoPage/ToDoTask/ToDoTaskActions.tsx
+++ apps/frontend/src/components/ToDoPage/ToDoTask/ToDoTaskActions.tsx
@@ -1,3 +1,4 @@
+import { ModalContext } from "@/context/ModalContext";
 import { ToDoContext } from "@/context/ToDoContext";
 import { IToDoTask } from "@/mock/mockToDoList";
 import { useContext } from "react";
@@ -5,16 +6,21 @@ import { AiFillCheckCircle, AiFillInfoCircle } from "react-icons/ai";
 
 export default function ToDoTaskActions({ taskData }: { taskData: IToDoTask }) {
   const { toggleTaskCompletion } = useContext(ToDoContext);
+  const { toggleModal } = useContext(ModalContext);
 
   return (
     <div className="flex justify-end items-center w-2/6 gap-x-2">
-      <button>
-        <div className="flex items-center justify-center size-6 border border-woodsmoke-950 rounded-full text-2xl text-woodsmoke-950">
+      <button
+        onClick={() => {
+          toggleModal("read");
+        }}
+      >
+        <div className="to-do-btn">
           <AiFillInfoCircle />
         </div>
       </button>
       <button onClick={() => toggleTaskCompletion(taskData)}>
-        <div className="flex items-center justify-center size-6 border border-woodsmoke-950 rounded-full text-2xl text-woodsmoke-950">
+        <div className="to-do-btn">
           {taskData.isCompleted && <AiFillCheckCircle />}
         </div>
       </button>
