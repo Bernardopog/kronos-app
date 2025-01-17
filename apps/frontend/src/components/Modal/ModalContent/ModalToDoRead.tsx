fb52a48d48c5c873bd37dd8152c642ff5f0a2bf6
R3ygoski <hckranonymous2@hotmail.com>
2025-01-17 10:18:19 -0300

    feat(frontend): add Task Priority to Task info modal

--- apps/frontend/src/components/Modal/ModalContent/ModalToDoRead.tsx
+++ apps/frontend/src/components/Modal/ModalContent/ModalToDoRead.tsx
@@ -38,14 +38,39 @@ export default function ModalToDoRead() {
           )}
         </div>
         <hr />
+        <div className="flex justify-between items-center">
+          <h3>Prioridade:</h3>
+          <div className="flex flex-col items-center min-w-24 p-1 border rounded-lg font-medium border-woodsmoke-200">
+            {selectedTask?.priority === "0" && "Nenhuma"}
+            {selectedTask?.priority === "1" && "Muito Baixa"}
+            {selectedTask?.priority === "2" && "Baixa"}
+            {selectedTask?.priority === "3" && "Media"}
+            {selectedTask?.priority === "4" && "Alta"}
+            {selectedTask?.priority === "5" && "Muito Alta"}
+
+            <div
+              className={`
+              flex items-center justify-center w-fit px-2 rounded-md font-bold
+              ${selectedTask?.priority === "0" && "bg-priority-none"}
+              ${selectedTask?.priority === "1" && "bg-priority-lowest"}
+              ${selectedTask?.priority === "2" && "bg-priority-lower"}
+              ${selectedTask?.priority === "3" && "bg-priority-medium"}
+              ${selectedTask?.priority === "4" && "bg-priority-higher"}
+              ${selectedTask?.priority === "5" && "bg-priority-highest"}
+            `}
+            >
+              {selectedTask?.priority}
+            </div>
+          </div>
+        </div>
         <div>
           <h3 className="font-bold">Descrição:</h3>
           <p
             className={`${!selectedTask?.description || selectedTask?.description?.trim() === "" ? "italic text-woodsmoke-950/50" : ""}`}
           >
             {!selectedTask?.description ||
-selectedTask?.description?.trim() === ""
-              ?               "Não há descrição..."
+            selectedTask?.description?.trim() === ""
+              ? "Não há descrição..."
               : selectedTask?.description}
           </p>
         </div>
