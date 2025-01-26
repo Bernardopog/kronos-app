edcc1096a3e74b96e7246f38355e3823e1955980
R3ygoski <hckranonymous2@hotmail.com>
2025-01-26 18:41:13 -0300

    feat(frontend): create Update Modal Content

---
+++ apps/frontend/src/components/Modal/ModalContent/ModalToDoUpdate.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import SelectRow from "@/components/Select/SelectRow";
+import { ToDoContext } from "@/context/ToDoContext";
+import { PriorityType } from "@/mock/mockToDoList";
+import { useContext, useState } from "react";
+import ModalFooter from "../ModalFooter";
+import { ModalContext } from "@/context/ModalContext";
+import Input from "@/components/Input/Input";
+import TextArea from "@/components/TextArea/TextArea";
+
+export default function ModalToDoUpdate() {
+  const { toggleModal } = useContext(ModalContext);
+  const { selectedTask, updateTask } = useContext(ToDoContext);
+
+  const [newTitle, setNewTitle] = useState<string>(selectedTask?.title ?? "");
+  const [newDescription, setNewDescription] = useState<string>(
+    selectedTask?.description ?? ""
+  );
+  const [newPriority, setNewPriority] = useState<PriorityType>(
+    selectedTask?.priority ?? "0"
+  );
+
+  return (
+    <>
+      <div className="flex flex-col p-2 py-6 gap-y-8 text-woodsmoke-950">
+        <Input
+          label="Título:"
+          id="title"
+          name="title"
+          value={newTitle}
+          setValue={setNewTitle}
+        />
+        <TextArea
+          label="Descrição:"
+          id="description"
+          value={newDescription}
+          setValue={setNewDescription}
+        />
+        <SelectRow value={newPriority} setValue={setNewPriority} />
+      </div>
+      <ModalFooter
+        type={"update"}
+        action={() => {
+          updateTask({
+            ...selectedTask!,
+            title: newTitle,
+            description: newDescription,
+            priority: newPriority,
+          });
+          toggleModal(null);
+        }}
+      />
+    </>
+  );
+}
