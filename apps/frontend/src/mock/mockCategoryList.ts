01eb845404bf1ad2a2c68f794c4e77db25f88c42
R3ygoski <hckranonymous2@hotmail.com>
2025-01-31 12:31:14 -0300

    feat(frontend): create a Mock Category List

---
+++ apps/frontend/src/mock/mockCategoryList.ts
@@ -0,0 +1,19 @@
+export interface ICategory {
+  id: string;
+  title: string;
+}
+
+export const mockCategoryList: ICategory[] = [
+  {
+    id: "aa628",
+    title: "Estudos",
+  },
+  {
+    id: "b996f",
+    title: "Pessoal",
+  },
+  {
+    id: "lop5a",
+    title: "Trabalho",
+  },
+];
