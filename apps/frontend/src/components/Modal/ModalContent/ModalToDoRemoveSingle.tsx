1014839ad64cf1334c26475aab95e903bbbd2b22
R3ygoski <hckranonymous2@hotmail.com>
2025-02-03 13:40:03 -0300

    style(frontend): change alert text from Remove Single Task and Remove Category

--- apps/frontend/src/components/Modal/ModalContent/ModalToDoRemoveSingle.tsx
+++ apps/frontend/src/components/Modal/ModalContent/ModalToDoRemoveSingle.tsx
@@ -17,7 +17,7 @@ export default function ModalToDoRemoveSingle() {
         </p>
         <p className="text-center">
           Cuidado, essa ação é{" "}
-          <span className="inline-block px-2 rounded-full font-bold text-poppy-600 bg-woodsmoke-950">
+          <span className="inline-block px-2 rounded-full font-bold text-poppy-600 dark:bg-woodsmoke-950">
             irreversível !
           </span>
         </p>
