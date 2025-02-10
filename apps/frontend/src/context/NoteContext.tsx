21ba80bf09b8dc5be703d55a69446ea871f745dd
R3ygoski <hckranonymous2@hotmail.com>
2025-02-10 09:39:19 -0300

    feat(frontend): create Select Note functionality to Note Context

--- apps/frontend/src/context/NoteContext.tsx
+++ apps/frontend/src/context/NoteContext.tsx
@@ -5,17 +5,26 @@ import { createContext, ReactNode, useState } from "react";
 
 interface INoteContext {
   noteList: INote[];
+  selectedNote: INote | null;
+  selectNote: (note: INote) => void;
 }
 
 const NoteContext = createContext({} as INoteContext);
 
 const NoteProvider = ({ children }: { children: ReactNode }) => {
   const [noteList, setNoteList] = useState<INote[]>(mockNoteList);
+  const [selectedNote, setSelectedNote] = useState<INote | null>(null);
+
+  const selectNote = (note: INote) => {
+    setSelectedNote(note);
+  };
 
   return (
     <NoteContext.Provider
       value={{
         noteList,
+        selectedNote,
+        selectNote,
       }}
     >
       {children}
