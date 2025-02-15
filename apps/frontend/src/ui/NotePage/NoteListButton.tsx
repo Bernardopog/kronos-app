87e7e6678a9d4ac45c0b75abef887a75607ca002
R3ygoski <hckranonymous2@hotmail.com>
2025-02-15 12:16:36 -0300

    feat(frontend): create Note List Button

---
+++ apps/frontend/src/ui/NotePage/NoteListButton.tsx
@@ -0,0 +1,24 @@
+"use client";
+import { NoteContext } from "@/context/NoteContext";
+import { useContext } from "react";
+import { AiOutlineUnorderedList } from "react-icons/ai";
+
+export default function NoteListButton() {
+  const { toggleList, isListShow } = useContext(NoteContext);
+
+  return (
+    <>
+      {!isListShow && (
+        <button
+          className="to-do-tabs-button left-0 rounded-r-xl"
+          onClick={() => {
+            toggleList("open");
+          }}
+          aria-label="Abrir filtro"
+        >
+          <AiOutlineUnorderedList />
+        </button>
+      )}
+    </>
+  );
+}
