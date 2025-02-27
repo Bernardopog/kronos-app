f8864a79b42a67620b1f89d2c29f90126ec13a36
R3ygoski <hckranonymous2@hotmail.com>
2025-02-27 13:01:33 -0300

    feat(frontend): add characters limit to note, delete note button and download note option

--- apps/frontend/src/components/NotePage/NoteMainBody.tsx
+++ apps/frontend/src/components/NotePage/NoteMainBody.tsx
@@ -31,7 +31,7 @@ export default function NoteMainBody({ selectedNote }: INoteMainBodyProps) {
       {editableDesc ? (
         <textarea
           className="
-          w-full min-h-[95%] p-2 rounded-lg bg-transparent scrollbar-thin scrollbar-track-transparent scrollbar-thumb-woodsmoke-900 ease-in-out duration-300 
+          w-full min-h-full p-2 rounded-lg bg-transparent scrollbar-thin scrollbar-track-transparent scrollbar-thumb-woodsmoke-900 ease-in-out duration-300 
           focus:outline focus:outline-woodsmoke-600
           dark:bg-woodsmoke-950 dark:scrollbar-thumb-woodsmoke-100
           dark:focus:outline-woodsmoke-100
@@ -41,13 +41,15 @@ export default function NoteMainBody({ selectedNote }: INoteMainBodyProps) {
           ref={descInputRef}
           value={noteDescription}
           onChange={(ev) => {
-            setNoteDescription(ev.target.value);
+            if (noteDescription.length > 20000) return;
+            else if (ev.target.value.length > 20000) return;
+            else setNoteDescription(ev.target.value);
           }}
           onBlur={() => {
             updateNote({ ...selectedNote, description: noteDescription });
             setEditableDesc(false);
           }}
-        ></textarea>
+        />
       ) : (
         <p
           onDoubleClick={() => {
--- apps/frontend/src/ui/NotePage/NoteOptions.tsx
+++ apps/frontend/src/ui/NotePage/NoteOptions.tsx
@@ -2,51 +2,69 @@
 
 import Button from "@/components/Button/Button";
 import TabCloseButton from "@/components/Button/TabCloseButton";
+import { ModalContext } from "@/context/ModalContext";
 import { NoteContext } from "@/context/NoteContext";
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import {
+  AiFillDelete,
   AiFillHeart,
   AiFillSetting,
   AiOutlineClose,
   AiOutlineHeart,
 } from "react-icons/ai";
+import { BsFiletypeTxt } from "react-icons/bs";
 
 export default function NoteOptions() {
   const { selectedNote, toggleFavorite, toggleOptions, optionsShowControl } =
     useContext(NoteContext);
+  const { toggleModal } = useContext(ModalContext);
 
   const [toggleOptionMenuDesktop, setToggleOptionMenuDesktop] = useState(false);
+  const [url, setUrl] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (url) {
+      return () => URL.revokeObjectURL(url);
+    }
+  }, [url]);
+
+  const handleDownload = () => {
+    const text = selectedNote!.description!;
+    const textBlob = new Blob([text], { type: "application/pdf" });
+    const url = URL.createObjectURL(textBlob);
+    setUrl(url);
+  };
 
   return (
     <>
-      <>
-        {selectedNote && (
-          <Button
-            extraStyles={{
-              button: `hidden fixed top-20 right-4 z-50 rounded-full bg-woodsmoke-200 
-                dark:border dark:bg-woodsmoke-950 
-                hover:border-woodsmoke-400
-                lg:flex`,
-              icon: `text-woodsmoke-950 dark:text-woodsmoke-50 ${toggleOptionMenuDesktop && "ease-in-out duration-1000 hover:rotate-180"}`,
-            }}
-            action={() => {
-              setToggleOptionMenuDesktop(!toggleOptionMenuDesktop);
-            }}
-            ariaLabel="Abrir opções"
-            icon={
-              !toggleOptionMenuDesktop ? <AiOutlineClose /> : <AiFillSetting />
-            }
-          />
-        )}
+      {selectedNote && (
+        <Button
+          extraStyles={{
+            button: `hidden fixed top-20 right-4 z-50 rounded-full bg-woodsmoke-200 
+              dark:border dark:bg-woodsmoke-950 
+              hover:border-woodsmoke-400
+              lg:flex`,
+            icon: `text-woodsmoke-950 dark:text-woodsmoke-50 ${toggleOptionMenuDesktop && "ease-in-out duration-1000 hover:rotate-180"}`,
+          }}
+          action={() => {
+            setToggleOptionMenuDesktop(!toggleOptionMenuDesktop);
+          }}
+          ariaLabel="Abrir opções"
+          icon={
+            !toggleOptionMenuDesktop ? <AiOutlineClose /> : <AiFillSetting />
+          }
+        />
+      )}
+      {selectedNote && (
         <aside
           className={`
-            fixed top-0 pt-4 size-full overflow-y-auto scrollbar-thin scrollbar-thumb-woodsmoke-950 scrollbar-track-transparent bg-woodsmoke-100 ease-in-out duration-300
-            dark:bg-woodsmoke-925 dark:shadow-side dark:shadow-woodsmoke-100/10
-            lg:static lg:translate-x-0 lg:bg-woodsmoke-50
-            ${!selectedNote && "hidden"}
-            ${optionsShowControl ? "-right-0" : "-right-full"}
-            ${toggleOptionMenuDesktop && "closed"}
-          `}
+          flex flex-col fixed top-0 size-full pt-4 px-2 gap-y-2 overflow-y-auto scrollbar-thin scrollbar-thumb-woodsmoke-950 scrollbar-track-transparent bg-woodsmoke-100 ease-in-out duration-300
+          dark:bg-woodsmoke-925 dark:shadow-side dark:shadow-woodsmoke-100/10
+          lg:static lg:translate-x-0 lg:bg-woodsmoke-50
+          ${!selectedNote && "hidden"}
+          ${optionsShowControl ? "-right-0" : "-right-full"}
+          ${toggleOptionMenuDesktop && "closed"}
+        `}
           id="nt-options"
         >
           <TabCloseButton
@@ -56,14 +74,14 @@ export default function NoteOptions() {
             ariaLabel="Fechar opções da nota"
             icon={<AiOutlineClose />}
           />
-          <div className="flex justify-between items-center p-2">
+          <div className="flex justify-between items-center">
             <Button
               extraStyles={{
                 button: `px-2 text-woodsmoke-800
-                  dark:text-woodsmoke-200
-                  dark:hover:shadow-btn
-                  a
-                  ${selectedNote?.isFavorite ? "hover:bg-poppy-600 dark:hover:shadow-poppy-600/25" : "hover:bg-apple-600 dark:hover:shadow-apple-600/25"}`,
+                dark:text-woodsmoke-200
+                dark:hover:shadow-btn
+                a
+                ${selectedNote?.isFavorite ? "hover:bg-poppy-600 dark:hover:shadow-poppy-600/25" : "hover:bg-apple-600 dark:hover:shadow-apple-600/25"}`,
                 label: "text-nowrap",
               }}
               ariaLabel={`${selectedNote?.isFavorite ? "Desfavoritar Nota" : "Favoritar Nota"}`}
@@ -74,8 +92,60 @@ export default function NoteOptions() {
               }
             />
           </div>
+          <div className="flex justify-between items-center">
+            <Button
+              extraStyles={{
+                button: `w-full px-2 text-woodsmoke-800
+                dark:text-woodsmoke-200
+                hover:bg-poppy-600
+                dark:hover:shadow-btn dark:hover:shadow-poppy-600/25`,
+                label: "text-nowrap",
+              }}
+              label={"Deletar Nota"}
+              action={() => {
+                toggleModal({
+                  headerTitle: "Deletar Nota",
+                  type: "delete",
+                  content: "noteDelete",
+                });
+              }}
+              icon={<AiFillDelete />}
+            />
+          </div>
+          {(selectedNote.description?.length ?? 0) > 0 && (
+            <div className="flex flex-col justify-center">
+              <p className="text-sm text-woodsmoke-800 ease-in-out duration-300 dark:text-woodsmoke-300">
+                Tamanho da Descrição:
+              </p>
+              <p
+                className="
+                  p-1 border rounded-lg text-center border-woodsmoke-300 text-woodsmoke-800 ease-in-out duration-300
+                  dark:border-woodsmoke-800 dark:text-woodsmoke-300
+                "
+              >
+                {selectedNote?.description?.length} /{" "}
+                <span className="opacity-50">20000</span>
+              </p>
+            </div>
+          )}
+          <a
+            className="
+                btn-base px-2 text-woodsmoke-800
+                dark:text-woodsmoke-200
+                hover:bg-apple-600
+                dark:hover:shadow-btn dark:hover:shadow-apple-600/25
+              "
+            href={url ?? "#"}
+            download={`note-${selectedNote.title}.txt`}
+            onClick={handleDownload}
+          >
+            <span className="text-xl">
+              <BsFiletypeTxt />
+            </span>
+            <p>Baixar Nota</p>
+          </a>
         </aside>
-      </>
+      )}
     </>
   );
 }
