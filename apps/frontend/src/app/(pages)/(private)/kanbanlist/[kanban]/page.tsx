f3e58cfd692fbac2c41c4c9cbf522a93f700f680
R3ygoski <hckranonymous2@hotmail.com>
2025-03-03 22:19:26 -0300

    feat(frontend): implement update and delete functionality to kanban page

--- apps/frontend/src/app/(pages)/(private)/kanbanlist/[kanban]/page.tsx
+++ apps/frontend/src/app/(pages)/(private)/kanbanlist/[kanban]/page.tsx
@@ -4,12 +4,17 @@ import KanbanColumn from "@/components/KanbanPage/KanbanColumn";
 import { KanbanContext } from "@/context/KanbanContext";
 import { ModalContext } from "@/context/ModalContext";
 import { useParams } from "next/navigation";
-import { useContext, useEffect } from "react";
-import { AiFillPlusCircle } from "react-icons/ai";
+import { useContext, useEffect, useState } from "react";
+import { AiFillDelete, AiFillPlusCircle } from "react-icons/ai";
 
 export default function Kanban() {
-  const { selectKanban, selectedKanban, columnList } =
-    useContext(KanbanContext);
+  const {
+    selectKanban,
+    selectedKanban,
+    columnList,
+    updateKanban,
+    deleteKanban,
+  } = useContext(KanbanContext);
   const { toggleModal } = useContext(ModalContext);
 
   const params = useParams();
@@ -17,17 +22,62 @@ export default function Kanban() {
 
   useEffect(() => {
     selectKanban(kanbanId);
+    setKanbanTitle(selectedKanban?.title ?? "");
   }, [kanbanId, columnList, selectedKanban, selectKanban]);
+
+  const [kanbanTitle, setKanbanTitle] = useState<string>("");
+  const [editingTitle, setEditingTitle] = useState<boolean>(false);
 
   return (
     <main
       id="main"
       className="page px-8 pb-4 flex flex-col gap-4 overflow-x-auto scrollbar-thin scrollbar-track-transparent scrollbar-thumb-woodsmoke-950"
     >
-      <header>
-        <h2 className="font-bold text-2xl text-woodsmoke-950 dark:text-woodsmoke-50 ease-in-out duration-300">
-          {selectedKanban?.title}
-        </h2>
+      <header className="flex items-center justify-between">
+        {editingTitle ? (
+          <h2
+            className="font-bold text-2xl text-woodsmoke-950 dark:text-woodsmoke-50 ease-in-out duration-300"
+            onClick={() => {
+              setEditingTitle(true);
+            }}
+          >
+            {selectedKanban?.title}
+          </h2>
+        ) : (
+          <input
+            type="text"
+            value={kanbanTitle}
+            className="w-[60%] font-bold text-2xl text-woodsmoke-950 dark:text-woodsmoke-50 bg-transparent ease-in-out duration-300 truncate"
+            onChange={(ev) => setKanbanTitle(ev.target.value)}
+            onBlur={() => {
+              updateKanban(kanbanTitle);
+              setEditingTitle(false);
+            }}
+            onKeyDown={(ev) => {
+              if (ev.key === "Enter") {
+                updateKanban(kanbanTitle);
+                setEditingTitle(false);
+              }
+            }}
+          />
+        )}
+        <Button
+          action={() => {
+            toggleModal({
+              content: "kanbanDelete",
+              type: "delete",
+              headerTitle: "Deletar Kanban",
+            });
+            deleteKanban(kanbanId);
+          }}
+          icon={<AiFillDelete />}
+          extraStyles={{
+            button: `text-woodsmoke-800
+              dark:text-woodsmoke-200 
+              hover:bg-poppy-600 
+              dark:hover:shadow-btn dark:hover:shadow-poppy-600/25`,
+          }}
+        />
       </header>
       <div className="flex h-full gap-4">
         {columnList
