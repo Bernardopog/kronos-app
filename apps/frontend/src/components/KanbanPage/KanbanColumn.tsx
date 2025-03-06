8165fc8e92a0a34dbc7296558d6e43c7ab3ece8b
R3ygoski <hckranonymous2@hotmail.com>
2025-03-05 22:02:20 -0300

    refactor(frontend): extract Kanban Column Header component

--- apps/frontend/src/components/KanbanPage/KanbanColumn.tsx
+++ apps/frontend/src/components/KanbanPage/KanbanColumn.tsx
@@ -1,14 +1,14 @@
 "use client";
 
 import Button from "../Button/Button";
-import { AiFillPlusCircle, AiFillSetting } from "react-icons/ai";
+import { AiFillPlusCircle } from "react-icons/ai";
 import { DragEvent, useContext, useState } from "react";
 import { KanbanContext } from "@/context/KanbanContext";
 import { IColumn } from "@/mock/kanban/mockKanbanColumns";
 import KanbanTask from "./KanbanTask";
 import { ModalContext } from "@/context/ModalContext";
 import KanbanColumnOption from "./KanbanColumnOption";
-import { icons } from "@/icons/icons";
+import KanbanColumnHeader from "./KanbanColumnHeader";
 
 interface IKanbanColumnProps {
   column: IColumn;
@@ -28,7 +28,7 @@ export default function KanbanColumn({
   dragDrop,
 }: IKanbanColumnProps) {
   const { toggleModal } = useContext(ModalContext);
-  const { taskList, updateColumn } = useContext(KanbanContext);
+  const { taskList } = useContext(KanbanContext);
 
   const [isOptionsOpen, setIsOptionsOpen] = useState<boolean>(false);
   const [isDragOver, setIsDragOver] = useState<boolean>(false);
@@ -41,7 +41,6 @@ export default function KanbanColumn({
     ev.preventDefault();
   };
 
-  const colorHeader = `hsla(${column.color?.[0]}, ${column.color?.[1]}%, ${column.color?.[2]}%, 0.75)`;
   const colorBody = `hsla(${column.color?.[0]}, ${column.color?.[1]}%, ${column.color?.[2]}%, 0.1)`;
 
   return (
@@ -59,68 +58,15 @@ export default function KanbanColumn({
       ${isDragOver && "shadow-btn shadow-woodsmoke-800/50"}
     `}
     >
-      <header
-        style={{ backgroundColor: `${colorHeader}` }}
-        className="flex items-center justify-between h-12 px-4 bg-woodsmoke-100 ease-in-out duration-300
-        dark:bg-woodsmoke-950
-        "
-      >
-        <div className="flex items-center gap-x-2">
-          {column.icon && (
-            <span
-              className={`text-2xl
-            ${(column.color?.[2] ?? 0) > 60 ? "text-woodsmoke-950 dark:text-woodsmoke-950" : "text-woodsmoke-50 dark:text-woodsmoke-50"}`}
-            >
-              {icons[column.icon as keyof typeof icons]}
-            </span>
-          )}
-          {isEditingColumnTitle ? (
-            <h2
-              className="text-xl font-medium text-woodsmoke-800 cursor-pointer
-          dark:text-woodsmoke-200 ease-in-out duration-300
-          "
-              onClick={() => setEditingIsColumnTitle(true)}
-            >
-              {column?.columnName}
-            </h2>
-          ) : (
-            <input
-              type="text"
-              value={columnName}
-              className={`w-[60%] font-bold text-2xl bg-transparent ease-in-out duration-300 truncate
-                ${(column.color?.[2] ?? 0) > 60 ? "text-woodsmoke-950 dark:text-woodsmoke-950" : "text-woodsmoke-50 dark:text-woodsmoke-50"}
-              `}
-              onChange={(ev) => setColumnName(ev.target.value)}
-              onBlur={() => {
-                updateColumn(column.id, { ...column, columnName: columnName });
-                setEditingIsColumnTitle(false);
-              }}
-              onKeyDown={(ev) => {
-                if (ev.key === "Enter") {
-                  updateColumn(column.id, {
-                    ...column,
-                    columnName: columnName,
-                  });
-                  setEditingIsColumnTitle(false);
-                }
-              }}
-            />
-          )}
-        </div>
-        <Button
-          action={() => setIsOptionsOpen(!isOptionsOpen)}
-          ariaLabel="Configurações da Coluna"
-          icon={<AiFillSetting />}
-          extraStyles={{
-            button: `
-              border rounded-full bg-woodsmoke-100 text-woodsmoke-900
-              hover:text-woodsmoke-925
-              dark:text-woodsmoke-200 dark:bg-woodsmoke-925
-              dark:hover:shadow-btn dark:hover:shadow-woodsmoke-100/25 dark:hover:text-woodsmoke-100
-          `,
-          }}
-        />
-      </header>
+      <KanbanColumnHeader
+        column={column}
+        columnName={columnName}
+        setColumnName={setColumnName}
+        isOptionsOpen={isOptionsOpen}
+        setIsOptionsOpen={setIsOptionsOpen}
+        isEditingColumnTitle={isEditingColumnTitle}
+        setEditingIsColumnTitle={setEditingIsColumnTitle}
+      />
       <KanbanColumnOption column={column} isOptionsOpen={isOptionsOpen} />
       <section
         style={{ backgroundColor: `${colorBody}` }}
---
+++ apps/frontend/src/components/KanbanPage/KanbanColumnHeader.tsx
@@ -0,0 +1,100 @@
+import { KanbanContext } from "@/context/KanbanContext";
+import { icons } from "@/icons/icons";
+import { IColumn } from "@/mock/kanban/mockKanbanColumns";
+import { Dispatch, useContext } from "react";
+import { AiFillSetting } from "react-icons/ai";
+import Button from "../Button/Button";
+
+interface IKanbanColumnHeaderProps {
+  column: IColumn;
+  columnName: string;
+  setColumnName: Dispatch<React.SetStateAction<string>>;
+  isOptionsOpen: boolean;
+  setIsOptionsOpen: Dispatch<React.SetStateAction<boolean>>;
+  isEditingColumnTitle: boolean;
+  setEditingIsColumnTitle: Dispatch<React.SetStateAction<boolean>>;
+}
+
+export default function KanbanColumnHeader({
+  column,
+  columnName,
+  setColumnName,
+  isEditingColumnTitle,
+  setEditingIsColumnTitle,
+  isOptionsOpen,
+  setIsOptionsOpen,
+}: IKanbanColumnHeaderProps) {
+  const { updateColumn } = useContext(KanbanContext);
+
+  const colorHeader = `hsla(${column.color?.[0]}, ${column.color?.[1]}%, ${column.color?.[2]}%, 0.75)`;
+
+  return (
+    <header
+      style={{
+        backgroundColor: `${colorHeader}`,
+      }}
+      className="flex items-center justify-between h-12 px-4 bg-woodsmoke-100 ease-in-out duration-300
+      dark:bg-woodsmoke-950
+      "
+    >
+      <div className="flex items-center gap-x-2">
+        {column.icon && (
+          <span
+            className={`text-2xl
+                ${(column.color?.[2] ?? 0) > 60 ? "text-woodsmoke-950 dark:text-woodsmoke-950" : "text-woodsmoke-50 dark:text-woodsmoke-50"}`}
+          >
+            {icons[column.icon as keyof typeof icons]}
+          </span>
+        )}
+        {isEditingColumnTitle ? (
+          <h2
+            className="text-xl font-medium text-woodsmoke-800 cursor-pointer
+              dark:text-woodsmoke-200 ease-in-out duration-300
+              "
+            onClick={() => setEditingIsColumnTitle(true)}
+          >
+            {column?.columnName}
+          </h2>
+        ) : (
+          <input
+            type="text"
+            value={columnName}
+            className={`w-[60%] font-bold text-2xl bg-transparent ease-in-out duration-300 truncate
+                    ${(column.color?.[2] ?? 0) > 60 ? "text-woodsmoke-950 dark:text-woodsmoke-950" : "text-woodsmoke-50 dark:text-woodsmoke-50"}
+                  `}
+            onChange={(ev) => setColumnName(ev.target.value)}
+            onBlur={() => {
+              updateColumn(column.id, {
+                ...column,
+                columnName: columnName,
+              });
+              setEditingIsColumnTitle(false);
+            }}
+            onKeyDown={(ev) => {
+              if (ev.key === "Enter") {
+                updateColumn(column.id, {
+                  ...column,
+                  columnName: columnName,
+                });
+                setEditingIsColumnTitle(false);
+              }
+            }}
+          />
+        )}
+      </div>
+      <Button
+        action={() => setIsOptionsOpen(!isOptionsOpen)}
+        ariaLabel="Configurações da Coluna"
+        icon={<AiFillSetting />}
+        extraStyles={{
+          button: `
+            border rounded-full bg-woodsmoke-100 text-woodsmoke-900
+            hover:text-woodsmoke-925
+            dark:text-woodsmoke-200 dark:bg-woodsmoke-925
+            dark:hover:shadow-btn dark:hover:shadow-woodsmoke-100/25 dark:hover:text-woodsmoke-100
+        `,
+        }}
+      />
+    </header>
+  );
+}
