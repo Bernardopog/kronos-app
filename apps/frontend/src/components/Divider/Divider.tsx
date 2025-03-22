ea14f17f3824664ffd4ef7b9337122737310e30f
R3ygoski <hckranonymous2@hotmail.com>
2025-03-22 00:13:59 -0300

    chore(frontend): add prop className to Divider component

--- apps/frontend/src/components/Divider/Divider.tsx
+++ apps/frontend/src/components/Divider/Divider.tsx
@@ -1,5 +1,8 @@
-export default function Divider() {
+export default function Divider({ className }: { className?: string }) {
   return (
-    <div className="w-full h-0.5 rounded-full bg-woodsmoke-200 dark:bg-woodsmoke-900 duration-300 ease-in-out"></div>
+    <div
+      className={`w-full h-0.5 rounded-full bg-woodsmoke-200 dark:bg-woodsmoke-900 duration-300 ease-in-out
+      ${className}`}
+    />
   );
 }
