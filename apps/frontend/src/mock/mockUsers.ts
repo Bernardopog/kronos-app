7cfba8332efcd96730a993763805c99e1b3d3fd5
R3ygoski <hckranonymous2@hotmail.com>
2025-03-22 00:04:26 -0300

    chore(frontend): create a Mock User List

---
+++ apps/frontend/src/mock/mockUsers.ts
@@ -0,0 +1,23 @@
+export interface IUser {
+  id: string;
+  username: string;
+  email: string;
+  password: string;
+  displayName?: string;
+}
+
+export const mockUserList: IUser[] = [
+  {
+    id: "123",
+    username: "user1",
+    email: "user1@email.com",
+    password: "12345678",
+    displayName: "John Doe",
+  },
+  {
+    id: "456",
+    username: "user2",
+    email: "user2@email.com",
+    password: "87654321",
+  },
+];
