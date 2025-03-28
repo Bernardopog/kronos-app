8e1bca74c6bc8022e4c35695de4f6348fd1f9380
R3ygoski <hckranonymous2@hotmail.com>
2025-03-28 14:21:59 -0300

    chore(backend): backend setup

---
+++ apps/backend/src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  app.enableCors({
+    origin: process.env.ORIGIN ?? 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+  });
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
