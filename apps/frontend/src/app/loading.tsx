2bca63d5f194cd340b106b60c1e8a743bd056074
R3ygoski <hckranonymous2@hotmail.com>
2025-04-04 16:52:00 -0300

    feat(frontend): create loading screen

---
+++ apps/frontend/src/app/loading.tsx
@@ -0,0 +1,13 @@
+export default function loading() {
+  return (
+    <main
+      className="flex items-center justify-center size-full bg-woodsmoke-200
+    dark:bg-woodsmoke-925"
+      id="main"
+    >
+      <div className="loader loader-gradient">
+        <p className="loader-title">K</p>
+      </div>
+    </main>
+  );
+}
